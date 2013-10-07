e26935a5f076ba857dc00848615095cf33ae23e1
peter942 <petoriaomalley@gmail.com>
2013-10-08 00:43:24 +0100

    Added Devise and made it look nicer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,8 @@ Pinteresting::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  #Set this to actual host name
+  config.action_mailer.default_url_options = { :host => 'http://summiterest.heroku.com/'}
+
 end
