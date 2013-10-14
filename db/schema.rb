f81a6733fe96e1436eb1a1820d18ce21dcd51caf
peter942 <petoriaomalley@gmail.com>
2013-10-14 19:21:53 +0100

    Added Unique Usernames to the User Model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131013132822) do
+ActiveRecord::Schema.define(version: 20131014171059) do
 
   create_table "pins", force: true do |t|
     t.string   "description"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20131013132822) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
